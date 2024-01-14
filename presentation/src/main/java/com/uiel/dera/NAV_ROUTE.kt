63f38e285506def45910e2385d454a4781ca63d6
uielPark <parkuiery@gmail.com>
2024-01-14 23:20:31 +0900

    🌱 :: 루트

--- presentation/src/main/java/com/uiel/dera/NAV_ROUTE.kt
+++ presentation/src/main/java/com/uiel/dera/NAV_ROUTE.kt
@@ -1,4 +1,9 @@
 package com.uiel.dera
 
-enum class NAV_ROUTE {
+enum class NAV_ROUTE(val routeName: String,val description: String) {
+    MAIN("MAIN","메인화면"),
+    SIGININ("SIGININ","로그인화면"),
+    SIGNUP("SIGNUP","회원가입화면"),
+    ALARM("ALARM","알람화면"),
+    WRITE("WRITE","작성화면")
 }

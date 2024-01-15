59fa2a4da1dddae77309c7de126847aef96f7053
uielPark <parkuiery@gmail.com>
2024-01-15 22:16:27 +0900

    ☁️ :: 코드 변경

--- presentation/src/main/java/com/uiel/dera/NAV_ROUTE.kt
+++ presentation/src/main/java/com/uiel/dera/NAV_ROUTE.kt
@@ -1,9 +1,9 @@
 package com.uiel.dera
 
-enum class NAV_ROUTE(val routeName: String,val description: String) {
-    MAIN("MAIN","메인화면"),
-    SIGININ("SIGININ","로그인화면"),
-    SIGNUP("SIGNUP","회원가입화면"),
-    ALARM("ALARM","알람화면"),
-    WRITE("WRITE","작성화면")
+enum class DeraScreen() {
+    MAIN,
+    SIGININ,
+    SIGNUP,
+    ALARM,
+    WRITE
 }

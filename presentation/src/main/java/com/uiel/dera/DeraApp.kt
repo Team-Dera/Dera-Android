491edb134c3382841d3d406e06827a7978d46b7d
uielPark <parkuiery@gmail.com>
2024-01-16 22:12:39 +0900

    ⚙️ :: 라이브러리

--- presentation/src/main/java/com/uiel/dera/DeraApp.kt
+++ presentation/src/main/java/com/uiel/dera/DeraApp.kt
@@ -1,4 +1,25 @@
 package com.uiel.dera
 
-class DeraApp {
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.uiel.dera.feature.signin.SignInScreen
+import com.uiel.dera.feature.signup.SignUpScreen
+
+@Composable
+fun DeraApp() {
+
+    val navController = rememberNavController()
+
+    NavHost(navController = navController, startDestination = Screen.SignIn.route) {
+        composable(Screen.SignIn.route) {
+            SignInScreen(navController)
+        }
+        composable(Screen.SignUp.route) {
+            SignUpScreen(navController)
+        }
+    }
 }

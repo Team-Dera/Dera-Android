01d74475b046f1a544d5b47e7c07ddd2ab06f69b
uielPark <parkuiery@gmail.com>
2024-01-17 22:33:21 +0900

    ☁️ :: 코드 변경

--- presentation/src/main/java/com/uiel/dera/DeraApp.kt
+++ presentation/src/main/java/com/uiel/dera/DeraApp.kt
@@ -1,8 +1,6 @@
 package com.uiel.dera
 
-import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController

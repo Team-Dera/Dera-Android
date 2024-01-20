7d52318563f554cc1e466ad399409d615463daf6
uiel <parkuiery@gmail.com>
2024-01-20 22:45:27 +0900

    🌱 :: 회원가입 화면

--- presentation/src/main/java/com/uiel/dera/feature/signup/SignUpScreen.kt
+++ presentation/src/main/java/com/uiel/dera/feature/signup/SignUpScreen.kt
@@ -1,4 +1,20 @@
 package com.uiel.dera.feature.signup
 
-class SignUpScreen {
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.navigation.NavController
+
+@Composable
+fun SignUpScreen(navController: NavController) {
+
+    Text(
+        text = "aaaaa",
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = Color.Blue)
+    )
 }

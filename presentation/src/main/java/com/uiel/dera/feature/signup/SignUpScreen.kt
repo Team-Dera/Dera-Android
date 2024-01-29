49c60a0dcad06a60f10ec45ea54911fb2ce205d3
uielPark <parkuiery@gmail.com>
2024-01-29 22:19:05 +0900

    feat :: 회원가입

--- presentation/src/main/java/com/uiel/dera/feature/signup/SignUpScreen.kt
+++ presentation/src/main/java/com/uiel/dera/feature/signup/SignUpScreen.kt
@@ -1,20 +1,157 @@
 package com.uiel.dera.feature.signup
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 
 @Composable
-fun SignUpScreen(navController: NavController) {
+fun SignUpScreen(
+    navController: NavController,
+    signUpScreenViewModel: SignUpScreenViewModel = viewModel()
+) {
+    val uiState by signUpScreenViewModel.signUpState.observeAsState()
 
-    Text(
-        text = "aaaaa",
+    Column(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        SignUpInputs(
+            name = { uiState?.name ?: "" },
+            id = { uiState?.id ?: "" },
+            password = { uiState?.password ?: "" },
+            onNameChanged = { signUpScreenViewModel.updateName(name = it)},
+            onIdChanged = {signUpScreenViewModel.updateId(id = it)},
+            onPwChanged = {signUpScreenViewModel.updatePassword(password = it)}
+        )
+        SignUpBtn(navController)
+    }
+
+}
+
+@Composable
+private fun SignUpInputs(
+    name: () -> String,
+    id: () -> String,
+    password: () -> String,
+    onNameChanged: (String) -> Unit,
+    onIdChanged: (String) -> Unit,
+    onPwChanged : (String) -> Unit
+) {
+    Box(
         modifier = Modifier
-            .fillMaxSize()
-            .background(color = Color.Blue)
-    )
+            .padding(
+                start = 20.dp,
+                end = 20.dp,
+                top = 100.dp
+            )
+            .fillMaxWidth()
+            .height(50.dp)
+            .border(
+                width = 1.dp,
+                color = Color.Black,
+                shape = RoundedCornerShape(8.dp)
+            )
+    ) {
+        BasicTextField(
+            value = name(),
+            onValueChange = onNameChanged,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(start = 8.dp)
+                .align(Alignment.CenterStart)
+        )
+        if (name().isBlank()) {
+            Text(
+                text = "이름",
+                modifier = Modifier
+                    .padding(start = 8.dp)
+                    .align(Alignment.CenterStart)
+            )
+        }
+    }
+    Box(
+        modifier = Modifier
+            .padding(
+                start = 20.dp,
+                end = 20.dp,
+                top = 40.dp
+            )
+            .fillMaxWidth()
+            .height(50.dp)
+            .border(
+                width = 1.dp,
+                color = Color.Black,
+                shape = RoundedCornerShape(8.dp)
+            )
+    ) {
+        BasicTextField(
+            value = id(),
+            onValueChange = onIdChanged,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(start = 8.dp)
+                .align(Alignment.CenterStart)
+        )
+        if (id().isBlank()) {
+            Text(
+                text = "아이디",
+                modifier = Modifier
+                    .padding(start = 8.dp)
+                    .align(Alignment.CenterStart)
+            )
+        }
+    }
+    Box(
+        modifier = Modifier
+            .padding(
+                start = 20.dp,
+                end = 20.dp,
+                top = 40.dp
+            )
+            .fillMaxWidth()
+            .height(50.dp)
+            .border(
+                width = 1.dp,
+                color = Color.Black,
+                shape = RoundedCornerShape(8.dp)
+            )
+    ) {
+        BasicTextField(
+            value = password(),
+            onValueChange = onPwChanged,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(start = 8.dp)
+                .align(Alignment.CenterStart)
+        )
+        if (password().isBlank()) {
+            Text(
+                text = "비밀번호",
+                modifier = Modifier
+                    .padding(start = 8.dp)
+                    .align(Alignment.CenterStart)
+            )
+        }
+    }
+}
+
+@Composable
+private fun SignUpBtn(navController: NavController) {
+
 }
--- presentation/src/main/java/com/uiel/dera/feature/signup/SignUpScreenViewModel.kt
+++ presentation/src/main/java/com/uiel/dera/feature/signup/SignUpScreenViewModel.kt
@@ -1,4 +1,28 @@
 package com.uiel.dera.feature.signup
 
-class SignUpScreenViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class SignUpScreenViewModel:ViewModel() {
+    private val _signUpState: MutableLiveData<SignUpState> = MutableLiveData(SignUpState())
+    val signUpState: LiveData<SignUpState> = _signUpState
+
+    fun updateName(name: String) {
+        _signUpState.postValue(signUpState.value?.copy(name = name))
+    }
+
+    fun updateId(id: String) {
+        _signUpState.postValue(signUpState.value?.copy(id = id))
+    }
+
+    fun updatePassword(password: String) {
+        _signUpState.postValue(signUpState.value?.copy(password = password))
+    }
 }
+
+data class SignUpState(
+    val name: String = "",
+    val id: String = "",
+    val password: String = ""
+)

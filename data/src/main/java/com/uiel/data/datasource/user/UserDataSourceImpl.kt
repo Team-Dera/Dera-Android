cde706aed31bccebd4138f74696ee281f7b9413c
uielPark <parkuiery@gmail.com>
2024-02-02 22:46:27 +0900

    refactor :: 코드 추가

--- data/src/main/java/com/uiel/data/datasource/user/UserDataSourceImpl.kt
+++ data/src/main/java/com/uiel/data/datasource/user/UserDataSourceImpl.kt
@@ -5,15 +5,20 @@ import com.uiel.data.model.signin.SignInRequest
 import com.uiel.data.model.signin.SignInResponse
 import com.uiel.data.model.signup.SignUpRequest
 import com.uiel.data.model.signup.SignUpResponse
+import com.uiel.data.util.ExceptionHandler
 
 class UserDataSourceImpl(
     private val userApi: UserApi,
 ) : UserDataSource {
-    override suspend fun postSignUp(request: SignUpRequest): SignUpResponse {
-        TODO("Not yet implemented")
-    }
+    override suspend fun postSignUp(
+        request: SignUpRequest,
+    ) = ExceptionHandler<SignUpResponse>().httpRequest {
+        userApi.postSignUp(request = request)
+    }.sendRequest()
 
-    override suspend fun postSignIn(request: SignInRequest): SignInResponse {
-        TODO("Not yet implemented")
-    }
+    override suspend fun postSignIn(
+        request: SignInRequest,
+    ) = ExceptionHandler<SignInResponse>().httpRequest {
+        userApi.postSignIn(request = request)
+    }.sendRequest()
 }

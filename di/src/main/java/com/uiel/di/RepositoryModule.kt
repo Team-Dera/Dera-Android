e4235bb67eb6a1c27fd9595e291eda2776b1ab40
uielPark <parkuiery@gmail.com>
2024-02-08 23:15:16 +0900

    feat :: RepositoryModule

---
+++ di/src/main/java/com/uiel/di/RepositoryModule.kt
@@ -0,0 +1,21 @@
+package com.uiel.di
+
+import com.uiel.data.repository.UserRepositoryImpl
+import com.uiel.domain.repository.UserRepository
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class RepositoryModule {
+
+    @Binds
+    @Singleton
+    abstract fun bindUserRepository(
+        userRepositoryImpl: UserRepositoryImpl,
+    ): UserRepository
+
+}

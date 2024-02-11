96267fd7867a6a4c8c610b2708bb2bfe703d6a75
uielPark <parkuiery@gmail.com>
2024-02-11 22:28:44 +0900

    refactor :: 코드 변경

--- presentation/src/main/java/com/uiel/dera/feature/signup/SignUpScreenViewModel.kt
+++ presentation/src/main/java/com/uiel/dera/feature/signup/SignUpScreenViewModel.kt
@@ -1,10 +1,30 @@
 package com.uiel.dera.feature.signup
 
+import androidx.annotation.StringRes
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.uiel.dera.feature.root.BaseViewModel
+import com.uiel.dera.util.mvi.SideEffect
+import com.uiel.dera.util.mvi.State
+import com.uiel.domain.exception.BadRequestException
+import com.uiel.domain.exception.UserAlreadyExists
+import com.uiel.domain.usecase.user.SignUpUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import org.orbitmvi.orbit.syntax.simple.intent
+import org.orbitmvi.orbit.syntax.simple.postSideEffect
+import org.orbitmvi.orbit.syntax.simple.reduce
+import org.orbitmvi.orbit.viewmodel.container
+import javax.inject.Inject
 
-class SignUpScreenViewModel:ViewModel() {
+@HiltViewModel
+class SignUpScreenViewModel @Inject constructor(
+    private val signUpUseCase: SignUpUseCase,
+) : BaseViewModel<SignUpState, SignUpSideEffect>() {
+
+    override val container = container<SignUpState, SignUpSideEffect>(SignUpState())
+
     private val _signUpState: MutableLiveData<SignUpState> = MutableLiveData(SignUpState())
     val signUpState: LiveData<SignUpState> = _signUpState
 
@@ -19,10 +39,71 @@ class SignUpScreenViewModel:ViewModel() {
     fun updatePassword(password: String) {
         _signUpState.postValue(signUpState.value?.copy(password = password))
     }
+
+    internal fun postSignUp() = intent {
+        viewModelScope.launch {
+            signUpUseCase(
+                userName = state.name,
+                accountId = state.id,
+                password = state.password,
+            ).onSuccess {
+                postSideEffect(SignUpSideEffect.MoveToMain)
+            }.onFailure { throwable ->
+                when (throwable) {
+                    is BadRequestException -> {
+                        reduce {
+                            state.copy(
+                                passwordError = true,
+                                signUpBtnEnabled = false,
+                            )
+                        }
+                    }
+
+                    is UserAlreadyExists -> {
+                        reduce {
+                            state.copy(
+                                idError = true,
+                                signUpBtnEnabled = false,
+                            )
+                        }
+                    }
+
+                    else -> {
+                        postSideEffect(
+                            SignUpSideEffect.Exception(
+                                message = getStringFormatException(
+                                    throwable = throwable,
+                                ),
+                            ),
+                        )
+                    }
+                }
+            }
+        }
+    }
+
+    private fun setSignUpBtnEnabled() = intent {
+        with(state) {
+            reduce {
+                copy(
+                    signUpBtnEnabled = id.isNotEmpty() && name.isNotEmpty()
+                            && password.isNotEmpty() && !idError && passwordError
+                )
+            }
+        }
+    }
 }
 
 data class SignUpState(
     val name: String = "",
     val id: String = "",
-    val password: String = ""
-)
+    val password: String = "",
+    val idError: Boolean = false,
+    val passwordError: Boolean = false,
+    val signUpBtnEnabled: Boolean = false
+) : State
+
+sealed class SignUpSideEffect : SideEffect {
+    object MoveToMain : SignUpSideEffect()
+    class Exception(@StringRes val message: Int) : SignUpSideEffect()
+}
